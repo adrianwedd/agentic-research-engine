1938ffb5bcdfdf2f80e085c8bc7f855c693615b5
Adrian Wedd <adrian@adrianwedd.com>
2025-06-15 21:55:08 +1000

    feat: add HTML Scraper tool

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,15 +1,14 @@
 """Tool package exposing callable wrappers for external services."""
 
+from . import web_search
+from .html_scraper import html_scraper
 from .ltm_client import consolidate_memory, retrieve_memory
 from .pdf_reader import pdf_extract
-from .web_search import web_search
 
 __all__ = [
     "consolidate_memory",
     "retrieve_memory",
-    "web_search", 
-    "pdf_extract"
+    "web_search",
+    "html_scraper",
+    "pdf_extract",
 ]
-
-
-
