29dfd957e555f44a50cad94dd2fa1322d0dd7267
Adrian Wedd <adrian@adrianwedd.com>
2025-06-16 05:58:42 +1000

    Integrate TRL PPO optimizer

---
+++ services/learning/__init__.py
@@ -0,0 +1,6 @@
+"""Learning utilities and optimizers."""
+
+from .rlaif_system import RLAIFSystem
+from .ppo_policy_optimizer import PPOPolicyOptimizer
+
+__all__ = ["RLAIFSystem", "PPOPolicyOptimizer"]
